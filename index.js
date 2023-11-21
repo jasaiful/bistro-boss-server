4ebe31123560eef906d95e57ac0e57628f7d828f
jasaiful <jasaiful@gmail.com>
2023-11-21 22:11:26 +0600

    modified part-5

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000
 
-
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -41,28 +40,28 @@ async function run() {
 
         // JWT middlewares
         const verifyToken = (req, res, next) => {
-            console.log('inside verify token', req.headers.authorization);
+            // console.log('inside verify token', req.headers.authorization);
             if (!req.headers.authorization) {
-                return res.status(401).send({ message: 'unauthorized access' });
+              return res.status(401).send({ message: 'unauthorized access' });
             }
-            const token = req.headers.authorization.split(' ')[1]
+            const token = req.headers.authorization.split(' ')[1];
             jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-                if (err) {
-                    return res.status(401).send({ message: 'unauthorized access' });
-                }
-                req.decoded = decoded;
-                next();
+              if (err) {
+                return res.status(401).send({ message: 'unauthorized access' })
+              }
+              req.decoded = decoded;
+              next();
             })
-        }
+          }
 
         // use verify admin after verifyToken
-        const verifyAdmin = async(req, res, next) => {
+        const verifyAdmin = async (req, res, next) => {
             const email = req.decoded.email;
-            const query = {email: email};
+            const query = { email: email };
             const user = await userCollection.findOne(query);
             const isAdmin = user?.role === 'admin';
-            if(!isAdmin){
-                return res.status(403).send({message: 'forbidden access'})
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' })
             }
             next();
         }
@@ -75,7 +74,7 @@ async function run() {
         });
 
         // verify admin
-        app.get('/user/admin/:email', verifyToken, async (req, res) => {
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
             const email = req.params.email;
             if (email !== req.decoded.email) {
                 return res.status(403).send({ message: 'forbidden access' })
